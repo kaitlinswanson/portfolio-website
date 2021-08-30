de1c483e1d68bb2af760bec51c729ef1c6fb43d3
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-29 18:12:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIW7NjKMQVQ3dy1sB6vxCvnm4gF1vPa5c",
+  authDomain: "portfolio-6f5ea.firebaseapp.com",
+  projectId: "portfolio-6f5ea",
+  storageBucket: "portfolio-6f5ea.appspot.com",
+  messagingSenderId: "832525904884",
+  appId: "1:832525904884:web:0cd71aa1506785171727a1",
+  measurementId: "G-26KEMGRLCB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Welcome to my portfolio! 
     </div>
   );
 }
