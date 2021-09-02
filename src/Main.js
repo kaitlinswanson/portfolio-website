a3f938d01f199bd8aab639a529b6988d968945b2
Kaitlin Swanson <kateswanson94@gmail.com>
2021-09-02 11:52:04 -0700

    photo and welcome message

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,12 @@ function Main() {
             <nav id="nav">
                 <div className="name">Kaitlin Swanson</div>
                 </nav>
-            <main id="main">Main</main>
+            <main id="main">
+            <div className="welcome">
+            <div id="welcome-message">Hi, I'm Kaitlin!</div>
+            <img id="profile-pic" src="animated-pic.gif" alt="profile pic"></img>
+            </div>
+            </main>
             <div id="content1">Content 1</div>
             <div id="content2">Content 2</div>
             <div id="content3">Content 3</div>
