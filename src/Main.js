6c318be8f0143491936cac7854a0f1605f69e848
Kaitlin Swanson <kateswanson94@gmail.com>
2021-09-02 13:53:57 -0700

    changed image and colors

--- src/Main.js
+++ src/Main.js
@@ -5,17 +5,26 @@ function Main() {
     return (
         <div className="main-holder">
             <nav id="nav">
-                <div className="name">Kaitlin Swanson</div>
+                <div className="name"></div>
                 </nav>
             <main id="main">
             <div className="welcome">
+            <div className="title">
             <div id="welcome-message">Hi, I'm Kaitlin!</div>
-            <img id="profile-pic" src="animated-pic.gif" alt="profile pic"></img>
+            <div className="socials-holder">
+                <div id="github"><i className="fab fa-github"></i></div> 
+                <div id="linkedin"><i className="fab fa-linkedin"></i></div>
+                <div id="twitter"><i className="fab fa-twitter"></i></div>
+                </div>
+            </div>
+            <img id="profile-pic" src="profile-pic.png" alt="profile pic"></img>
             </div>
             </main>
-            <div id="content1">Content 1</div>
-            <div id="content2">Content 2</div>
-            <div id="content3">Content 3</div>
+            <div className="content-class" id="content1">
+                <div className="projct-title">Memory Game</div>
+            </div>
+            <div className="content-class" id="content2">Content 2</div>
+            <div className="content-class" id="content3">Content 3</div>
             <footer id="footer">Footer</footer>
         </div>
         
